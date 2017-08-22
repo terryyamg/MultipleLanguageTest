a8a11edd2c2311af613adfc3f806b69400581f97
Terry Yang <terry.yang@kooppi.com>
2017-08-22 16:46:03 +0800

    Update
    Update

--- MultipleLanguageTest/ViewController.swift
+++ MultipleLanguageTest/ViewController.swift
@@ -9,10 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labMessage: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,36 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnEnglish(_ sender: UIButton) {
+        UserDefaults.standard.set("Base", forKey: "lang") //儲存語系為基本
+        setMessage()
+    }
+    
+    @IBAction func btnJapan(_ sender: UIButton) {
+        UserDefaults.standard.set("ja", forKey: "lang") //儲存語系為日文
+        setMessage()
+    }
+    
+    @IBAction func btnChinses(_ sender: UIButton) {
+        UserDefaults.standard.set("zh-Hant", forKey: "lang") //儲存語系為繁體中文
+        setMessage()
+    }
+    
+    func setMessage() {
+        labMessage.text = "A Song of Ice and Fire".localized //使用擴展功能 localized
+    }
 
 }
 
+extension String{
+
+    var localized: String {
+                
+        let lang = UserDefaults.standard.string(forKey: "lang")
+        
+        let bundle = Bundle(path: Bundle.main.path(forResource: lang, ofType: "lproj")!)
+
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+        
+    }
+}
